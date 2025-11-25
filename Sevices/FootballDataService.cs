b6b4fd21b48fff40910fcb8f379b5afcb4f92b56
Rafael Santana <rafa03santana@gmail.com>
2025-11-25 00:36:01 -0300

    instalando pacotes e consumindo API externa

---
+++ Sevices/FootballDataService.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace TopVsBottom.Api.Services
+{
+    public class FootballDataService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly IConfiguration _config;
+
+        public FootballDataService(HttpClient httpClient, IConfiguration config)
+        {
+            _httpClient = httpClient;
+            _config = config;
+            _httpClient.DefaultRequestHeaders.Add("X-Auth-Token", _config["FootballData:ApiKey"]);
+        }
+
+        public async Task<JObject> GetStandings(string leagueCode)
+        {
+            var url = $"{_config["FootballData:BaseUrl"]}/competitions/{leagueCode}/standings";
+            var json = await _httpClient.GetStringAsync(url);
+            return JObject.Parse(json);
+        }
+
+        public async Task<JObject> GetMatches(string leagueCode)
+        {
+            var url = $"{_config["FootballData:BaseUrl"]}/competitions/{leagueCode}/matches";
+            var json = await _httpClient.GetStringAsync(url);
+            return JObject.Parse(json);
+        }
+    }
+}
